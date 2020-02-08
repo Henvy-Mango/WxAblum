4a2358da49bf9d10daaf93e5a44335e3c9426916
Mangogo <807082193@qq.com>
2020-02-08 18:01:52 +0800

    修复找不到下载图片的bug
    另修改了样式，添加了查看文件夹的功能

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   stsUrl: 'https://pic.naomi.pub/authorization/sts.php',
   Bucket: 'naomi-1257008753',
   Region: 'ap-guangzhou',
-  albumDir: '',
+  Delimiter: '',
+  Prefix: '',
 };
