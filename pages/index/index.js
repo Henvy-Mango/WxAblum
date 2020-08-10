4d7a6a0b706df11f2b4ef31fdb62d7af557b1b19
Mangogo <807082193@qq.com>
2020-08-10 22:55:46 +0800

    canvas 2D 接口
    index页面使用成功，
    album页面获取不到实例，正在解决,
    另添加promise库

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,9 @@ const app = getApp();
 var cos = app.globalData.cos;
 
 Page({
-  data: {},
+  data: {
+    message: {}
+  },
   onLoad: function () { },
   onShareAppMessage: function (res) {
     return {
@@ -13,6 +15,7 @@ Page({
       path: this.route,
     }
   },
+
   // 前往相册页
   uploadImage() {
     var that = this
@@ -21,50 +24,57 @@ Page({
       camera: 'back',
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
-      success: function (res) {
-        wx.showLoading({
-          title: '正在上传...',
-        });
-        var filePath = res.tempFilePaths[0];
-        that.getCanvasImg(res.tempFilePaths).then(res => {
-          util.checkSafePic(res).then(
-            res => {
-              if (res) {
-                //图片正常
-                if (filePath) {
-                  var Key = util.getRandFileName(filePath);
-                  cos.postObject({
-                    Bucket: config.Bucket,
-                    Region: config.Region,
-                    Key: Key,
-                    FilePath: filePath,
-                  }, function (err, data) {
-                    wx.hideLoading();
-                    if (data && data.Location) {
-                      wx.navigateTo({
-                        url: '../preview/preview?type=image&url=' + encodeURIComponent('https://' + data.Location)
-                      });
-                    } else {
-                      wx.showToast({
-                        title: '上传失败',
-                        icon: 'error',
-                        duration: 3000
-                      });
-                    }
+    }).then(res => {
+      var filePath = res.tempFilePaths[0];
+      that.getCanvasDetail(filePath)
+        .then(res => that.getCanvasImg(res))
+        .then(res => util.checkSafePic(res))
+        .then(res => {
+          if (res) {
+            //图片正常
+            if (filePath) {
+              var Key = util.getRandFileName(filePath);
+              cos.postObject({
+                Bucket: config.Bucket,
+                Region: config.Region,
+                Key: Key,
+                FilePath: filePath,
+              }, function (err, data) {
+                that.setData({
+                  message: {
+                    enable: true,
+                    type: "info",
+                    text: "正在上传图片…",
+                    delay: 3000,
+                  },
+                })
+                if (data && data.Location) {
+                  wx.navigateTo({
+                    url: '../preview/preview?type=image&url=' + encodeURIComponent('https://' + data.Location)
                   });
+                } else {
+                  that.setData({
+                    message: {
+                      enable: true,
+                      type: "error",
+                      text: "上传失败",
+                      delay: 3000,
+                    },
+                  })
                 }
-              } else {
-                wx.showToast({
-                  title: '图片违法违规',
-                  icon: 'none',
-                  duration: 3000
-                });
-              }
+              });
             }
-          )
-        }
-        )
-      }
+          } else {
+            that.setData({
+              message: {
+                enable: true,
+                type: "error",
+                text: "图片违规违法",
+                delay: 3000,
+              },
+            })
+          }
+        })
     })
   },
   // 前往相册页
@@ -111,13 +121,14 @@ Page({
       url: '../album/album'
     });
   },
-  //压缩并获取图片，这里用了递归的方法来解决canvas的draw方法延时的问题
-  getCanvasImg: function (tempFilePaths) {
+
+  // 计算图片缩小后的尺寸
+  getCanvasDetail: function (tempFilePaths) {
     var that = this;
     return new Promise(function (resolve, reject) {
       //-----返回选定照片的本地文件路径列表，获取照片信息-----------
       wx.getImageInfo({
-        src: tempFilePaths[0],
+        src: tempFilePaths,
         success: function (res) {
           //---------利用canvas压缩图片--------------
           var ratio = 2;
@@ -129,34 +140,58 @@ Page({
             ratio++;
           }
           console.log("图片压缩后大小为" + canvasWidth + "x" + canvasHeight)
-          that.setData({
-            cWidth: canvasWidth,
-            cHeight: canvasHeight
-          })
 
-          //----------绘制图形并取出图片路径--------------
-          var ctx = wx.createCanvasContext('canvas')
-          ctx.drawImage(res.path, 0, 0, canvasWidth, canvasHeight)
-          ctx.draw(false, setTimeout(function () {
-            wx.canvasToTempFilePath({
-              canvasId: 'canvas',
-              destWidth: canvasWidth,
-              destHeight: canvasHeight,
-              success: function (res) {
-                //最终图片路径
-                resolve(res.tempFilePath)
-              },
-              fail: function (res) {
-                console.log(res.errMsg)
-              }
-            })
-          }, 100))
-        }, //留一定的时间绘制canvas
+          that.setData({
+            canvasWidth: canvasWidth,
+            canvasHeight: canvasHeight
+          })
+          resolve(tempFilePaths)
+        },
         fail: function (res) {
           console.log(res.errMsg)
           reject(res)
         }
       })
     })
+  },
+
+  // 使用canvas画布，获取压缩后的图片
+  async getCanvasImg(res) {
+    var that = this;
+    //----------绘制图形并取出图片路径--------------
+    // 通过 SelectorQuery 获取 Canvas 节点
+    const query = wx.createSelectorQuery();
+
+    const canvas = await new Promise((resolve, reject) => {
+      query.select('#canvas')
+        .fields({ node: true, size: true })
+        .exec(async (item) => {
+          const width = item[0].width
+          const height = item[0].height
+
+          const canvas = item[0].node
+          const ctx = canvas.getContext('2d')
+
+          const dpr = wx.getSystemInfoSync().pixelRatio
+          canvas.width = width * dpr
+          canvas.height = height * dpr
+          ctx.scale(dpr, dpr)
+
+          const image = canvas.createImage();//创建image       
+          image.src = res;//指定路径为getImageInfo的文件
+          image.onload = () => {
+            ctx.drawImage(image, 0, 0, width, height)//图片加载完成时draw
+            resolve(canvas)
+          }
+        })
+    })
+    return wx.canvasToTempFilePath({
+      canvas: canvas,
+      quality: 0.8,
+    }, that).then(res => {
+      console.log(res.tempFilePath)
+      return res.tempFilePath
+    })
   }
+
 })
