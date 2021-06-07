102754a8759dee30a752dd8f6b6ab750deced0d6
Naomi <807082193@qq.com>
2021-06-07 19:09:31 +0800

    主页菜单接口

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   stsUrl: 'https://pic.naomi.pub/authorization/sts.php',
+  menuUrl: 'https://pic.naomi.pub/authorization/menu.json',
   Bucket: 'naomi-1257008753',
   Region: 'ap-guangzhou',
   Delimiter: '',
