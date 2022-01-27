296fa9c9ab6a53f3e252c888af5710b8896f3e82
Naomi <mangogo.naomi@qq.com>
2022-01-27 16:57:52 +0800

    style: ESLint fix

--- pages/album/uploader/uploader.js
+++ pages/album/uploader/uploader.js
@@ -1,12 +1,12 @@
 import {
-  getRandFileName
-} from "../../../lib/util";
+  getRandFileName,
+} from '../../../lib/util';
 
 import {
   checkSafePic,
   uploadPic,
-  getDir
-} from "../../../lib/api"
+  getDir,
+} from '../../../lib/api';
 
 import Notify from '../../../miniprogram_npm/@vant/weapp/notify/notify';
 
@@ -17,7 +17,7 @@ Page({
 
     // 按钮
     loading: false,
-    message: "上传",
+    message: '上传',
     flag: false,
 
     toolBar: {
@@ -28,7 +28,7 @@ Page({
   },
 
   onLoad() {
-    this.getAlbumDir()
+    this.getAlbumDir();
   },
 
   // 获取文件夹
@@ -36,51 +36,51 @@ Page({
     let that = this;
 
     var {
-      toolBar
-    } = this.data
+      toolBar,
+    } = this.data;
 
     getDir(/^(?!.*CDN).*$/).then((res) => {
-      toolBar.folder = ['/'].concat(res)
+      toolBar.folder = ['/'].concat(res);
       that.setData({
-        toolBar
-      })
-    })
+        toolBar,
+      });
+    });
   },
 
   // 文件夹选择
   bindPickerChange(e) {
     var {
-      toolBar
-    } = this.data
-    console.log('picker发送选择改变，当前文件夹为', toolBar.folder[e.detail.value])
+      toolBar,
+    } = this.data;
+    console.log('picker发送选择改变，当前文件夹为', toolBar.folder[e.detail.value]);
     toolBar.selectFolder = e.detail.value;
     this.setData({
-      toolBar
-    })
+      toolBar,
+    });
   },
 
   beforeRead(event) {
     const {
       callback,
-      file
+      file,
     } = event.detail;
 
     var {
-      fileList = []
+      fileList = [],
     } = this.data;
 
-    var tmpList = [...fileList, ...file].filter(item => item.size < 20971520)
+    var tmpList = [...fileList, ...file].filter(item => item.size < 20971520);
 
-    this.notifyMessage('primary', `安全检查中`, 2000)
+    this.notifyMessage('primary', '安全检查中', 2000);
 
     this.setData({
-      canvas: tmpList.map(item => {
+      canvas: tmpList.map(() => {
         return {
           canvasWidth: 0,
           canvasHeight: 0,
-        }
-      })
-    })
+        };
+      }),
+    });
 
     // forEach异步问题解决方案，使用map和Promise.all
     const checkTasks = tmpList.map(item => item.url)
@@ -90,143 +90,143 @@ Page({
         if (/\.(jpg|png|gif|jpeg|pjp|pjpeg|jfif|xbm|tif|svgz|webp|ico|bmp|svg)$/.test(extName)) {
           return this.getCanvasDetail(filePath, index)
             .then(res => this.getCanvasImg(res))
-            .then(res => checkSafePic(res))
+            .then(res => checkSafePic(res));
         } else {
-          return Promise.resolve(true)
+          return Promise.resolve(true);
         }
-      })
+      });
 
     Promise.all(checkTasks).then(async (res) => {
       // 计数器
-      var times = 0
+      var times = 0;
 
       await res.map((item, index) => {
         if (!item) {
-          console.log(`--------第${index+1}张图片违规--------`)
-          times++
-          tmpList[index].status = 'failed'
-          tmpList[index].message = "图片违规"
+          console.log(`--------第${index+1}张图片违规--------`);
+          times++;
+          tmpList[index].status = 'failed';
+          tmpList[index].message = '图片违规';
         } else {
-          tmpList[index].status = 'done'
+          tmpList[index].status = 'done';
         }
-      })
+      });
 
       this.setData({
-        fileList: tmpList
-      })
+        fileList: tmpList,
+      });
 
       if (times != 0)
-        this.notifyMessage('warning', `共 ${times} 张图片违规`)
-    })
+        this.notifyMessage('warning', `共 ${times} 张图片违规`);
+    });
 
-    callback(true)
+    callback(true);
   },
 
   afterRead(event) {
     const {
-      file
+      file,
     } = event.detail;
 
     var {
-      fileList = []
+      fileList = [],
     } = this.data;
 
     fileList.push.apply(fileList, file.map(item => {
       return {
         url: item.url,
-        status: 'uploading' // uploading表示上传中，failed表示上传失败，done表示上传完成
-      }
-    }))
+        status: 'uploading', // uploading表示上传中，failed表示上传失败，done表示上传完成
+      };
+    }));
     this.setData({
-      fileList
+      fileList,
     });
   },
 
   deleteImg(event) {
-    let index = event.detail.index
+    let index = event.detail.index;
     var {
-      fileList = []
+      fileList = [],
     } = this.data;
     fileList.splice(index, 1);
     this.setData({
-      fileList
+      fileList,
     });
   },
 
   overSizeImg(event) {
-    var index = event.detail.index
-    this.notifyMessage('warning', `第${index+1}张体积过大`, 3000)
+    var index = event.detail.index;
+    this.notifyMessage('warning', `第${index+1}张体积过大`, 3000);
   },
 
-  uploadList(event) {
+  uploadList() {
     var {
       fileList = [],
-        toolBar
-    } = this.data
+        toolBar,
+    } = this.data;
 
     if (fileList.length == 0) {
-      this.notifyMessage('warning', `请先添加图片`)
-      return
+      this.notifyMessage('warning', '请先添加图片');
+      return;
     }
 
     var banList = fileList.filter(item => {
       if (item.status == 'uploading') {
-        this.notifyMessage('primary', `请等待图片检测`)
+        this.notifyMessage('primary', '请等待图片检测');
         return true;
       } else if (item.status == 'failed') {
-        this.notifyMessage('warning', `图片违规`)
+        this.notifyMessage('warning', '图片违规');
         return true;
       }
-    })
+    });
 
     if (banList.length != 0) {
-      console.error("违规操作!!！")
-      return
+      console.error('违规操作!!！');
+      return;
     }
 
     this.setData({
-      loading: true
-    })
+      loading: true,
+    });
 
     // forEach异步问题解决方案，使用map和Promise.all
     const uploadTasks = fileList.filter(item => item.status == 'done')
       .map((item, index, arr) => {
-        arr[index].status = 'uploading'
+        arr[index].status = 'uploading';
         this.setData({
-          fileList: arr
-        })
+          fileList: arr,
+        });
         var Key = toolBar.selectFolder == 0 ? getRandFileName(item.url) : toolBar.folder[toolBar.selectFolder] + getRandFileName(item.url);
         return uploadPic(Key, item.url).then((res) => {
           if (res.statusCode == 200) {
-            arr[index].status = 'done'
+            arr[index].status = 'done';
           } else {
-            arr[index].status = 'failed'
-            arr[index].message = '上传失败'
+            arr[index].status = 'failed';
+            arr[index].message = '上传失败';
           }
           this.setData({
-            fileList: arr
-          })
+            fileList: arr,
+          });
           return res;
-        })
-      })
+        });
+      });
 
     Promise.all(uploadTasks)
       .then((data) => {
-        console.log(data)
+        console.log(data);
         this.setData({
           loading: false,
-          message: "上传成功，清空后重新上传",
-          flag: true
-        })
-        this.notifyMessage('success', `上传成功`)
-      })
+          message: '上传成功，清空后重新上传',
+          flag: true,
+        });
+        this.notifyMessage('success', '上传成功');
+      });
   },
 
-  refreshList(event) {
+  refreshList() {
     this.setData({
       fileList: [],
       flag: false,
-      message: "上传"
+      message: '上传',
     });
   },
 
@@ -240,82 +240,81 @@ Page({
         success: (res) => {
           //---------利用canvas压缩图片--------------
           var ratio = 2;
-          var canvasWidth = res.width //图片原始长宽
-          var canvasHeight = res.height
+          var canvasWidth = res.width; //图片原始长宽
+          var canvasHeight = res.height;
           while (canvasWidth > 100 || canvasHeight > 100) { // 保证宽高在400以内
-            canvasWidth = Math.trunc(res.width / ratio)
-            canvasHeight = Math.trunc(res.height / ratio)
+            canvasWidth = Math.trunc(res.width / ratio);
+            canvasHeight = Math.trunc(res.height / ratio);
             ratio++;
           }
-          console.log("图片压缩后大小为" + canvasWidth + "x" + canvasHeight)
+          console.log('图片压缩后大小为' + canvasWidth + 'x' + canvasHeight);
 
           const {
-            canvas
-          } = that.data
+            canvas,
+          } = that.data;
 
-          canvas[index].canvasWidth = canvasWidth
-          canvas[index].canvasHeight = canvasHeight
+          canvas[index].canvasWidth = canvasWidth;
+          canvas[index].canvasHeight = canvasHeight;
           that.setData({
-            canvas
-          })
+            canvas,
+          });
           resolve({
             canvasWidth: canvasWidth,
             canvasHeight: canvasHeight,
             index: index,
-            path: tempFilePaths
-          })
+            path: tempFilePaths,
+          });
         },
         fail: (res) => {
-          console.log(res.errMsg)
-          reject(res)
-        }
-      })
-    })
+          console.log(res.errMsg);
+          reject(res);
+        },
+      });
+    });
   },
 
   // 使用canvas画布，获取压缩后的图片
-  async getCanvasImg(res, callback) {
-    var that = this;
+  async getCanvasImg(res) {
     //----------绘制图形并取出图片路径--------------
     const query = wx.createSelectorQuery();
-    const canvas = await new Promise((resolve, reject) => {
+    const canvas = await new Promise((resolve) => {
       query.select('#canvas-' + res.index)
         .fields({
           node: true,
-          size: true
+          size: true,
         })
         .exec(async (item) => {
-          const width = item[0].width
-          const height = item[0].height
+          const width = item[0].width;
+          const height = item[0].height;
 
-          const canvas = item[0].node
-          const ctx = canvas.getContext('2d')
+          const canvas = item[0].node;
+          const ctx = canvas.getContext('2d');
 
-          const dpr = wx.getSystemInfoSync().pixelRatio
-          canvas.width = width * dpr
-          canvas.height = height * dpr
-          ctx.scale(dpr, dpr)
+          const dpr = wx.getSystemInfoSync().pixelRatio;
+          canvas.width = width * dpr;
+          canvas.height = height * dpr;
+          ctx.scale(dpr, dpr);
 
           const image = canvas.createImage(); //创建image       
           image.src = res.path; //指定路径为getImageInfo的文件
           image.onload = () => {
-            ctx.drawImage(image, 0, 0, width, height) //图片加载完成时draw
-            resolve(canvas)
-          }
-        })
-    })
+            ctx.drawImage(image, 0, 0, width, height); //图片加载完成时draw
+            resolve(canvas);
+          };
+        });
+    });
 
     const tempFilePath = await wx.canvasToTempFilePath({
       canvas: canvas,
       quality: 0.5,
-      fileType: "jpg",
+      fileType: 'jpg',
       width: canvas.width,
       height: canvas.height,
       destWidth: canvas.width,
       destHeight: canvas.height,
-    }).then((res) => res.tempFilePath)
+    }).then((res) => res.tempFilePath);
 
-    return tempFilePath
+    return tempFilePath;
   },
 
   notifyMessage(type, text, duration = 3000) {
@@ -323,7 +322,7 @@ Page({
       type: type,
       duration: duration,
       message: text,
-      safeAreaInsetTop: false
+      safeAreaInsetTop: false,
     });
   },
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,12 @@
 import {
-  formatTime
-} from '../../lib/util'
+  formatTime,
+} from '../../lib/util';
 
 import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
 
-const {
-  getMenu
-} = require('../../lib/api');
+import {
+  getMenu,
+} from '../../lib/api';
 
 // 页面目前需要从服务端获取的数据：公告栏内容tip
 Page({
@@ -15,108 +15,108 @@ Page({
     navigationList: [{
       id: 1,
       name: '上传图片',
-      bindEvent: 'uploadImg'
+      bindEvent: 'uploadImg',
     }],
     navigateHeight: 100, // 单位:rpx
   },
 
   onLoad: function () {
-    var that = this
-    that.getTip()
+    var that = this;
+    that.getTip();
   },
 
   // 开启图片预览
-  ClickImg: function (e) {
+  ClickImg: function () {
     wx.previewImage({
       urls: [this.data.tip.img],
-    })
+    });
   },
 
   // 获取公告
   getTip: function () {
-    var that = this
+    var that = this;
     getMenu().then((res) => {
       var {
-        navigationList
-      } = this.data
+        navigationList,
+      } = this.data;
       var {
         album,
-        announcement
-      } = res.data
+        announcement,
+      } = res.data;
       var tip = {
         img: announcement.photoUrl,
         msg: announcement.message,
-        date: formatTime(new Date(res.header["Last-Modified"])),
-      }
+        date: formatTime(new Date(res.header['Last-Modified'])),
+      };
       if (album.enable) {
         navigationList.push({
           id: navigationList.length + 1,
-          name: album.bindName != "" ? album.bindName : '云相册',
-          bindEvent: album.bindEvent != "" ? album.bindEvent : 'goToAlbum'
-        })
+          name: album.bindName != '' ? album.bindName : '云相册',
+          bindEvent: album.bindEvent != '' ? album.bindEvent : 'goToAlbum',
+        });
       }
       that.setData({
         tip,
-        navigationList
-      })
-    })
+        navigationList,
+      });
+    });
   },
 
   // 点击跳转按钮时执行的动画
   touchAminate(id) {
-    var that = this
+    var that = this;
     that.animate('#id' + id, [{
         opacity: 1.0,
-        backgroundColor: '#ffffff'
+        backgroundColor: '#ffffff',
       },
       {
         opacity: 0.8,
-        backgroundColor: '#cacaca'
+        backgroundColor: '#cacaca',
       },
       {
         opacity: 1.0,
-        backgroundColor: '#ffffff'
-      }
+        backgroundColor: '#ffffff',
+      },
     ], 50, function () {
       that.clearAnimation('#id' + id, {
-        opacity: true
-      })
-    })
+        opacity: true,
+      });
+    });
   },
 
   // 上传图片
   uploadImg(e) {
-    var that = this
-    that.touchAminate(e.currentTarget.dataset.item.id)
+    var that = this;
+    that.touchAminate(e.currentTarget.dataset.item.id);
     setTimeout(function () {
       // 上传图片
       wx.navigateTo({
-        url: '../album/uploader/uploader'
+        url: '../album/uploader/uploader',
       });
-    }, 50)
+    }, 50);
   },
 
   // 上传视频
   uploadVedio(e) {
-    var that = this
-    that.touchAminate(e.currentTarget.dataset.item.id)
+    var that = this;
+    that.touchAminate(e.currentTarget.dataset.item.id);
     setTimeout(function () {
       // 上传视频
       wx.navigateTo({
-        url: '../album/uploader/uploader'
+        url: '../album/uploader/uploader',
       });
-    }, 50)
+    }, 50);
   },
 
   // 前往相册页
   goToAlbum(e) {
-    var that = this
-    that.touchAminate(e.currentTarget.dataset.item.id)
+    var that = this;
+    that.touchAminate(e.currentTarget.dataset.item.id);
     setTimeout(function () {
       wx.navigateTo({
-        url: '../album/album'
+        url: '../album/album',
       });
-    }, 50)
+    }, 50);
   },
 
   // 查看作者
@@ -125,10 +125,10 @@ Page({
       // 弹窗
       Dialog.alert({
         title: 'Naomi在这',
-        message: 'CDN流量只有5GB，\n请不要当网盘使用qwq'
+        message: 'CDN流量只有5GB，\n请不要当网盘使用qwq',
       }).then(() => {
         // on close
       });
-    }, 50)
-  }
-})
+    }, 50);
+  },
+});
