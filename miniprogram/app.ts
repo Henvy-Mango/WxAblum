97919ac7f3aa8fbad44262ed3618796060b06ba7
Naomi <mangogo.naomi@qq.com>
2022-02-16 09:53:16 +0800

    feat: migrate foundation
    trying to migrate as follow
    - wxss
    - api
    - configruation

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -2,13 +2,12 @@ import { getSign } from './lib/api';
 
 App({
   globalData: {
-    cos: null,
+    cos: getSign(),
   },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    this.globalData.cos = getSign();
     wx.cloud.init();
     console.log(wx.getSystemInfoSync());
   },
